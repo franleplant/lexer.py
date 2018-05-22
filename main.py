89740b70c16d5b4b392a8a990091550a987631b7
Fran Guijarro <franleplant@gmail.com>
2018-05-21 23:19:51 -0300

    a more structured approach

--- main.py
+++ main.py
@@ -30,9 +30,29 @@ m: List[Any] = [
 class LexExcepction(Exception):
     pass
 
-
 TokenKind = str
-Token = Tuple[TokenKind, str, int, int]
+class Token:
+    def __init__(self, token_kind: TokenKind, lexeme: str, line: int, col: int) -> None:
+        self.kind = token_kind
+        self.lexeme = lexeme
+        self.line = line
+        self.col = col
+
+
+
+class PotentialToken:
+    def __init__(self, startIndex: int, line: int, lineBase: int) -> None:
+        self.start = startIndex
+        self.line = line
+        self.lineBase = lineBase
+
+    def col(self) -> int:
+        return self.start - self.lineBase
+
+
+    def to_token(self, token_kind: TokenKind, word: str) -> Token:
+        return Token(token_kind, word, self.line, self.col())
+
 
 
 
@@ -50,23 +70,26 @@ def get_token_candidates(word: str) -> List[TokenKind]:
     ]
 
 
+MAX_ITERATIONS = 200
 # TODO make diagram
 def lex(src: str) -> List[Token]:
     state = 0
     index = 0
     line = 1
     lineBase = 0
-    partial_token = None
+    potential_token = None
     tokens = []
 
-    max = 0
+    iter_count = 0
     while index <= len(src) + 1:
-        # TODO make this more explicit when it fails
-        if max > 45:
+        if iter_count > MAX_ITERATIONS:
+            print(('ERROR MAX_ITERATIONS'))
             break
         c = get_char(index, src)
-        print(('index', index, 'c', c, 'state', state, 'partial_token', partial_token))
-        print(('tokens', tokens))
+        # print(('index', index, 'c', c, 'state', state, 'potential_token', potential_token))
+        # print(('tokens', tokens))
+
+        # Initial White space skip
         if state == 0:
             if c.isspace():
                 index += 1
@@ -75,15 +98,15 @@ def lex(src: str) -> List[Token]:
                     lineBase = index
             else:
                 state = 1
-                partial_token = (index, line, lineBase)
+                potential_token = PotentialToken(index, line, lineBase)
 
+        # While being at least one potential accepted token consume characters
         elif state == 1:
-            (wordBegin, _, _) = partial_token
-            # print(('1 partial_token', partial_token))
-            word = src[wordBegin:index + 1]
-            # print(('1 word', word))
+            word = src[potential_token.start:index + 1]
             token_candidates = get_token_candidates(word)
-            print(('1: token_candidates', token_candidates))
+            # print(('1 potential_token', potential_token))
+            # print(('1 word', word))
+            # print(('1: token_candidates', token_candidates))
             is_accepted = len(token_candidates) > 0
             if is_accepted and not c.isspace():
                 index += 1
@@ -92,12 +115,11 @@ def lex(src: str) -> List[Token]:
                 index -= 1
                 # and move to the token generation state
                 state = 2
+
+        # Max length token detected, create one and restart the process
         elif state == 2:
-            # TODO better data structure
-            (wordBegin, line, lineBase) = partial_token
-            word = src[wordBegin:index + 1]
-            # print(('partial_token', partial_token))
-            # print(('wordBegin', wordBegin))
+            word = src[potential_token.start:index + 1]
+            # print(('potential_token', potential_token))
             # print(('word', word))
             token_candidates = get_token_candidates(word)
             if len(token_candidates) == 0:
@@ -105,12 +127,10 @@ def lex(src: str) -> List[Token]:
                 # TODO better error message
                 print('Unrecognized token')
                 break
-            # print(('prev_token_candidates', prev_token_candidates))
+
             token_kind = token_candidates[0]
-            col = wordBegin - lineBase
-            # TODO better data structure
-            token = (token_kind, word, line, col)
-            print(('new token', token))
+            token = potential_token.to_token(token_kind, word)
+            # print(('new token', token))
             tokens.append(token)
 
             index += 1
@@ -118,7 +138,7 @@ def lex(src: str) -> List[Token]:
         else:
             state = -1
 
-        max +=1
+        iter_count +=1
 
 
     error = state == 1
@@ -130,7 +150,7 @@ def printTokens(tokens: List[Token]):
                                                  "column")))
     print("------------------------------------------------------------")
     for token in tokens:
-        print("{:^10} {:^10} {:^10} {:^10}".format(*token))
+        print("{:^10} {:^10} {:^10} {:^10}".format(token.kind, token.lexeme, token.line, token.col))
 
     print("++++++++++++++++++++++")
     print(src)
